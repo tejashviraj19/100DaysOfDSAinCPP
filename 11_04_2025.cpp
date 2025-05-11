5f541834c3cbac0dbeba5faa01a20d845f3dbc4e
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-05-11 17:22:38 +0530

    Create 11_04_2025.cpp

---
+++ 11_04_2025.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 3, 2, 1, 4, 1, 3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    unordered_map<int, int> freq;
+    
+    for(int i = 0; i < n; ++i) {
+        freq[arr[i]]++;
+    }
+
+    cout << "Element Frequency:\n";
+    for(auto pair : freq) {
+        cout << pair.first << " : " << pair.second << endl;
+    }
+
+    return 0;
+}
