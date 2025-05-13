cfbf935d4a637be53913f5cf7440cce8dc4c1db7
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-05-13 16:57:52 +0530

    Create 13_05_2025.cpp

---
+++ 13_05_2025.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size) {
+    int start = 0, end = size - 1;
+
+    while (start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, size);
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
