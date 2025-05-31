05fbac8544077c9afdbe5519840b54d75912b406
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-06-01 01:41:26 +0530

    Create 01_06_2025.cpp

---
+++ 01_06_2025.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int num) {
+    int original = num, reversed = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    return original == reversed;
+}
+
+int main() {
+    int number = 121;
+
+    if (isPalindrome(number))
+        cout << number << " is a palindrome." << endl;
+    else
+        cout << number << " is not a palindrome." << endl;
+
+    return 0;
+}
