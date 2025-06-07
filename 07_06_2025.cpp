a785f9339b84ebbbce19a1933de3f941e0d12957
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-06-08 02:35:34 +0530

    Create 07_06_2025.cpp

---
+++ 07_06_2025.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0)
+            return false;
+    return true;
+}
+
+int main() {
+    int N = 50;
+
+    cout << "Prime numbers up to " << N << ": ";
+    for (int i = 2; i <= N; i++) {
+        if (isPrime(i))
+            cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
