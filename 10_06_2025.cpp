20d85f8c460589a4b5a38613898a50385f2aa34f
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-06-10 00:53:28 +0530

    10_06_2025.cpp

---
+++ 10_06_2025.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int countDigit(int number, int digit) {
+    int count = 0;
+    while (number) {
+        if (number % 10 == digit)
+            count++;
+        number /= 10;
+    }
+    return count;
+}
+
+int main() {
+    int number = 122334, digit = 3;
+
+    cout << "Digit " << digit << " occurs " << countDigit(number, digit) << " times." << endl;
+
+    return 0;
+}
