1ea15e12e56e6a68e6f2f5b73cea94f6e088bac5
Tejashvi Raj <tejashviraj2022@vitbhopal.ac.in>
2025-06-13 19:25:50 +0530

    Create 12_06_2025.cpp

--- 12_06_2025.cpp
+++ 12_06_2025.cpp
@@ -1,1 +1,18 @@
-jshcjh
+#include <iostream>
+using namespace std;
+
+int findMax(int arr[], int size) {
+    int maxVal = arr[0];
+    for (int i = 1; i < size; i++)
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+    return maxVal;
+}
+
+int main() {
+    int arr[] = {10, 25, 30, 50, 5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Maximum element: " << findMax(arr, size) << endl;
+    return 0;
+}
